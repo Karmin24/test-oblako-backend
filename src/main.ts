29f9d1d96ab58913412213d2894372f209d98837
Nikita <kar24mul@gmail.com>
2022-07-20 13:50:48 +0300

    added cheks

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  console.log(process.env.PORT);
+  console.log("Before start");
+
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
